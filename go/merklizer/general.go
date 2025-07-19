5ae5353aaf0273ecbeb993648af215ff73b08dc4
Adam Scrivener <aascrivener@gmail.com>
2025-07-19 22:50:20 +0200

    page justification and leaf page stuff

--- go/merklizer/general.go
+++ go/merklizer/general.go
@@ -2,6 +2,7 @@ package merklizer
 
 import (
 	"bytes"
+	"math/bits"
 
 	"github.com/ascrivener/jam/constants"
 )
@@ -68,6 +69,79 @@ func trace(blobs [][]byte, index int, hash func([]byte) [32]byte) [][]byte {
 	}
 }
 
+// (E.4)
+func constantDepthBinaryMerkleFn(blobs [][]byte, hash func([]byte) [32]byte) [32]byte {
+	constancyHashes := constancyPreprocessor(blobs, hash)
+	return [32]byte(node(hashSliceToByteSlice(constancyHashes), hash))
+}
+
+// (E.5)
+func pageJustification(blobs [][]byte, height, index int, hashFn func([]byte) [32]byte) [][32]byte {
+	constancyHashes := constancyPreprocessor(blobs, hashFn)
+	traceSlice := trace(hashSliceToByteSlice(constancyHashes), index<<height, hashFn)[:calcRemainingHeight(len(constancyHashes), height)]
+	return byteSliceToHashSlice(traceSlice)
+}
+
+// calcRemainingHeight computes max(0, ⌈log2(max(1,|v|))−x⌉)
+func calcRemainingHeight(blobsLen, height int) int {
+	// max(1, |v|)
+	maxLen := blobsLen
+	if maxLen < 1 {
+		maxLen = 1
+	}
+
+	// For 0 and 1, log2 ceiling is 0
+	if maxLen <= 1 {
+		return 0
+	}
+
+	// bits.Len(uint(n)) returns the minimum number of bits required to represent n,
+	// which equals floor(log2(n)) + 1 for n > 0
+	// For any value m, bits.Len(m-1) equals ceiling(log2(m)) when m > 1
+	log2Ceil := bits.Len(uint(maxLen - 1))
+
+	return max(0, log2Ceil-height)
+}
+
+// (E.6)
+func leavesPage(blobs [][]byte, height, index int, hashFn func([]byte) [32]byte) [][32]byte {
+	startIdx := index << height
+	endIdx := min(startIdx+(1<<height), len(blobs))
+	leaves := make([][32]byte, endIdx-startIdx)
+	for i := startIdx; i < endIdx; i++ {
+		leaves[i-startIdx] = hashFn(append([]byte("leaf"), blobs[i]...))
+	}
+	return leaves
+}
+
+// (E.7)
+func constancyPreprocessor(blobs [][]byte, hash func([]byte) [32]byte) [][32]byte {
+	hashes := make([][32]byte, nextPowerOfTwo(len(blobs)))
+	for i := range hashes {
+		if i < len(blobs) {
+			hashes[i] = hash(append([]byte("leaf"), blobs[i]...))
+		} else {
+			hashes[i] = [32]byte{}
+		}
+	}
+	return hashes
+}
+
+func nextPowerOfTwo(n int) int {
+	// If n is already a power of 2, return it
+	if n > 0 && (n&(n-1)) == 0 {
+		return n
+	}
+
+	// Find the position of the most significant bit
+	power := 1
+	for power < n {
+		power <<= 1
+	}
+
+	return power
+}
+
 func WellBalancedBinaryMerkle(blobs [][]byte, hash func([]byte) [32]byte) [32]byte {
 	if len(blobs) == 1 {
 		return hash(blobs[0])
@@ -106,4 +180,20 @@ func PagedProofsFromSegments(segments [][constants.SegmentSize]byte) [][constant
 
 func JustificationFromProofPage(proofPage [constants.SegmentSize]byte, segmentIndex uint16, segment [constants.SegmentSize]byte, erasureRoot [32]byte) ([][32]byte, bool) {
 	return [][32]byte{}, false
+}
+
+func hashSliceToByteSlice(fixed [][32]byte) [][]byte {
+	result := make([][]byte, len(fixed))
+	for i, f := range fixed {
+		result[i] = f[:]
+	}
+	return result
+}
+
+func byteSliceToHashSlice(bytes [][]byte) [][32]byte {
+	result := make([][32]byte, len(bytes))
+	for i, b := range bytes {
+		copy(result[i][:], b)
+	}
+	return result
 }
