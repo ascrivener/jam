c05d0b613b5760af9e71aff55cc71daf22b2525c
Adam Scrivener <aascrivener@gmail.com>
2025-08-04 16:20:34 +0500

    read and write hf bug fix

--- src/go/pkg/pvm/hostfunctions.go
+++ src/go/pkg/pvm/hostfunctions.go
@@ -133,14 +133,9 @@ func Read(ctx *HostFunctionContext[struct{}], serviceAccount *serviceaccount.Ser
 		var preImage *[]byte
 
 		if a != nil {
-			// Create key by hashing service ID and memory contents
-			serviceIdBytes := serializer.EncodeLittleEndian(4, uint64(sStar))
 			keyBytes := ctx.State.RAM.InspectRange(uint64(ko), uint64(kz), ram.NoWrap, false)
-			combinedBytes := append(serviceIdBytes, keyBytes...)
-
 			var keyArray [32]byte
-			h := blake2b.Sum256(combinedBytes)
-			copy(keyArray[:], h[:])
+			copy(keyArray[:], keyBytes)
 
 			// Look up in state if available
 			val, ok, err := a.GetServiceStorageItem(keyArray)
@@ -193,14 +188,9 @@ func Write(ctx *HostFunctionContext[struct{}], serviceAccount *serviceaccount.Se
 			return NewSimpleExitReason(ExitPanic), nil
 		}
 
-		// Compute the key hash
-		serviceIdBytes := serializer.EncodeLittleEndian(4, uint64(serviceIndex))
 		keyBytes := ctx.State.RAM.InspectRange(uint64(ko), uint64(kz), ram.NoWrap, false)
-		combinedBytes := append(serviceIdBytes, keyBytes...)
-
 		var keyArray [32]byte
-		h := blake2b.Sum256(combinedBytes)
-		copy(keyArray[:], h[:])
+		copy(keyArray[:], keyBytes)
 
 		// Determine 'l' - length of previous value if it exists, NONE otherwise
 		var l types.Register
